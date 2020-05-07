ede95bd4c1ab3c00219769e637b2e1c0f8aa1a0f
Joseph Asante <freecoding1@gmail.com>
2020-05-07 23:54:00 +0000

    navigation redered successfully at the first time

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+
+import OnlineShoppingScreen from './src/screens/OnlineShoppingScreen';
+import PaymentSuccessfulScreen from './src/screens/PaymentSuccessfulScreen';
+import AddToCartScreen from './src/screens/AddToCartScreen';
+
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName="OnlineShopping"
+                screenOptions={{
+                  headerTitleAlign:"center",
+                }}
+        >
+          <Stack.Screen 
+          options={{
+            title:"My Shop "
+          }}
+          name="OnlineShopping" component={OnlineShoppingScreen} />
+          <Stack.Screen name="AddToCart" component={AddToCartScreen} />
+          <Stack.Screen name="PaymentSuccessful" component={PaymentSuccessfulScreen} />
+          
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
